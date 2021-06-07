4eec12a87fdad231649e4ff81cdb599a9d859d7a
rifqicode <muhammadrifqi.tb@gmail.com>
2021-06-07 13:28:02 +0700

    fix ecommerce

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -18,7 +18,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-
+            @include("layouts.admin-navigation")
             <!-- Page Content -->
             <main>
                 {{ $slot }}
